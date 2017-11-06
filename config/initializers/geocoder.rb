0b4fed503a9b47e45641ce73a1be9e5b0881f334
Alfred Rowe <alfred@encodevlabs.com>
2017-11-06 18:08:28 +0000

    Removed LocationIQ Keys [Resetting]

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -7,7 +7,7 @@ Geocoder.configure(
     # use_https: false,           # use HTTPS for lookup requests? (if supported)
     # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
     # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
-    api_key: '997ee06595ecbe', # API key for geocoding service
+    api_key: ENV['GEO_API_KEY'], # API key for geocoding service
     cache: Redis.new,                 # cache object (must respond to #[], #[]=, and #del)
     cache_prefix: 'geo:',  # prefix (string) to use for all cache keys
 

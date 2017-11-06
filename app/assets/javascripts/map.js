8388cb960e0f81e4cb7e000021e1311062212d4e
Alfred Rowe <alfred@encodevlabs.com>
2017-11-06 12:17:28 +0000

    JS Events

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,11 +1,72 @@
-function display_map(el) {
-    var map = new GMaps({
-        el: el,
-        lat: -12.043333,
-        lng: -77.028333
+var map = null;
+
+function display_map() {
+    geo_locate();
+}
+
+function geo_locate() {
+    GMaps.geolocate({
+        success: function(position) {
+            map.setCenter(position.coords.latitude, position.coords.longitude);
+        },
+        error: function(error) {
+            console.log('Geolocation failed: '+error.message);
+        },
+        not_supported: function() {
+            alert("Your browser does not support geolocation");
+        },
+        always: function() {
+            // alert("Done!");
+        }
+    });
+}
+
+function search() {
+    GMaps.geocode({
+        address: $('#address').val() + ' Ghana',
+        callback: function(results, status) {
+            if (status == 'OK') {
+                var latlng = results[0].geometry.location;
+                map.setCenter(latlng.lat(), latlng.lng());
+                map.addMarker({
+                    lat: latlng.lat(),
+                    lng: latlng.lng()
+                });
+            }else{
+                alert(status);
+            }
+        }
+    });
+}
+
+function bind_address_search(){
+    $('#address').bind("enterKey",function(e){
+        search();
+    });
+    $('#address').keyup(function(e){
+        if(e.keyCode == 13)
+        {
+            $(this).trigger("enterKey");
+        }
     });
 }
 
 $(document).on('turbolinks:load', function(){
-   display_map('.godas-map');
+    map = new GMaps({
+        el: '.godas-map',
+        lat: 5.554929,
+        lng: -0.200690
+    });
+    display_map();
+    bind_address_search();
+    GMaps.on('click', map.map, function(event) {
+        var index = map.markers.length;
+        var lat = event.latLng.lat();
+        var lng = event.latLng.lng();
+        map.addMarker({
+            lat: lat,
+            lng: lng,
+            title: 'Marker #' + index
+        });
+    });
 });
